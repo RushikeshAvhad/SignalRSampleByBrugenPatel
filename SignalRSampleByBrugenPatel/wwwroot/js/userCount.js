b34aa8450b8d6f80a5952473233bd5eb9564d11f
rushi-smb <rushi@smbios.co.in>
2023-03-20 13:00:04 +0530

    Connect to SignalR Hub from client.js, call signalR hub method, Client receive update from SignalR hub and perform action

--- SignalRSampleByBrugenPatel/wwwroot/js/userCount.js
+++ SignalRSampleByBrugenPatel/wwwroot/js/userCount.js
@@ -3,9 +3,24 @@
 var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
 
 //  Connect to methods that hub invokes aka receive notifications from hub
-
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
 
 //  Invoke hub methods aka send notification to hub
-
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
 
 //  Start Connection
+function fulfilled() {
+    //  do something on start
+    console.log("Connection to User Hub Successful");
+}
+
+function rejected() {
+    //  Rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
