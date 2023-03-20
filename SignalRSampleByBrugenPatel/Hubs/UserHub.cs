2ecdb04a35b65f18d5b992cc09a15d987383b241
rushi-smb <rushi@smbios.co.in>
2023-03-20 11:06:15 +0530

    Add method to Hub and invoke method in client JS to notify clients

--- SignalRSampleByBrugenPatel/Hubs/UserHub.cs
+++ SignalRSampleByBrugenPatel/Hubs/UserHub.cs
@@ -4,5 +4,17 @@ namespace SignalRSampleByBrugenPatel.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded()
+        {
+            //  Whenever new tab opens call this method & increase totalViews
+            TotalViews++;
+
+            //  Send update to all clients that total views have been updated
+            //  Clients is inside the Hub Implementation
+            //  updateTotalViews Method located on client side
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
     }
 }
