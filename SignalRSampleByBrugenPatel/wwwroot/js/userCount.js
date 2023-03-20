9462c4975f0fd001beec0000cfb31e2d2c43db20
rushi-smb <rushi@smbios.co.in>
2023-03-20 12:34:23 +0530

    Create Connection with SignalR hub

---
+++ SignalRSampleByBrugenPatel/wwwroot/js/userCount.js
@@ -0,0 +1,11 @@
+﻿
+//  Create Connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//  Connect to methods that hub invokes aka receive notifications from hub
+
+
+//  Invoke hub methods aka send notification to hub
+
+
+//  Start Connection
